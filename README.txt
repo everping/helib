5eac83258624c8f4b61c61d40c330bd7016f8719
ThaiHungN <hungnguyenhsgs@gmail.com>
2022-09-09 18:59:31 +0700

    update server

--- README.txt
+++
@@ -1,7 +0,0 @@
-tách 2 file
-1 file client và 1 file server
-client gửi query và server trả lời
-
-code C chỉ dùng để mã hóa
-giao tiếp có thể dùng python
-
--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import datetime
-  
+import subprocess
+from flask import request
+
+
 x = datetime.datetime.now()
   
 # Initializing flask app
@@ -9,16 +12,21 @@ app = Flask(__name__)
   
 # Route for seeing a data
 @app.route('/search')
-def get_time():
+def search():
     # exec binary file to get output
-    
+    country_name = request.args.get('country', default = '*', type = str).strip()
+
+    cmd = "~/Desktop/HElib_demo/server/bgv_country_db_lookup/BGV_country_db_lookup db_filename=./bgv_country_db_lookup/countries_dataset.csv <<< " + country_name + " > ./log/log.txt"
+
+
+
+    subprocess.call(['bash', '-c', cmd])
+    search_res = open("./log/log.txt", "r").read().split("Query result: ")[-1].split("\u0000")[0]
+
     # return output to client
     return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
+        'search_result': search_res
+    }
   
       
 # Running app
